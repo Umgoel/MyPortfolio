8bd38fd17558189c90a8bb025a53e9025fd8a960
Umang Goel <umanggoel27@gmail.com>
2024-02-22 22:48:58 +0530

    removed a few comments

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,7 @@ import Experience from "./components/Experience/Experience";
 import Projects from "./components/Projects/Projects";
 import Contact from "./components/Contact/Contact";
 
-//Add a photogrpahy page
-//Blog about why running
-//
+
 function App() {
   return (
     <div className={styles.App}>
